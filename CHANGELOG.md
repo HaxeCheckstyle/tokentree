bc2915b8a1389b1ed87a40a7c7eb7dbcd269c2ca
AlexHaxe <Alexander.Blum@gmail.com>
2018-07-25 19:34:51 +0200

    added getArrowType (#70)
    * added getArrowType

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@
 - Added `TokenTreeCheckUtils.getDocComment()` [#69](https://github.com/HaxeCheckstyle/tokentree/issues/69)
 - Added support for structural extension [#49](https://github.com/HaxeCheckstyle/tokentree/issues/49)
 - Added support for `final` [#55](https://github.com/HaxeCheckstyle/tokentree/issues/55) + [#56](https://github.com/HaxeCheckstyle/tokentree/issues/56) + [#61](https://github.com/HaxeCheckstyle/tokentree/issues/61)
+- Added `TokenTreeCheckUtils.getArrowType()` [#70](https://github.com/HaxeCheckstyle/tokentree/issues/70)
 - Fixed running out of tokens during package and imports [#28](https://github.com/HaxeCheckstyle/tokentree/issues/28)
 - Fixed `function` in typedef body [#30](https://github.com/HaxeCheckstyle/tokentree/issues/30)
 - Fixed handling of `switch this {` [#34](https://github.com/HaxeCheckstyle/tokentree/issues/34)
--- test/tokentree/utils/TokenTreeCheckUtilsTest.hx
+++ test/tokentree/utils/TokenTreeCheckUtilsTest.hx
@@ -1,6 +1,7 @@
 package tokentree.utils;
 
 import tokentree.utils.TokenTreeCheckUtils.BrOpenType;
+import tokentree.utils.TokenTreeCheckUtils.ArrowType;
 import tokentree.TokenTreeBuilderParsingTest.TokenTreeBuilderParsingTests;
 import haxe.PosInfos;
 import massive.munit.Assert;
@@ -50,6 +51,36 @@ class TokenTreeCheckUtilsTest {
 		}
 	}
 
+	@Test
+	public function testArrowFunctions() {
+		var root:TokenTree = assertCodeParses(TokenTreeCheckUtilsTests.ARROW_FUNCTIONS);
+
+		var allArrows:Array<TokenTree> = root.filter([Arrow], ALL);
+		for (ar in allArrows) {
+			Assert.areEqual(ArrowType.ARROW_FUNCTION, TokenTreeCheckUtils.getArrowType(ar));
+		}
+	}
+
+	@Test
+	public function testFunctionTypeHaxe3() {
+		var root:TokenTree = assertCodeParses(TokenTreeCheckUtilsTests.FUNCTION_TYPE_HAXE_3);
+
+		var allArrows:Array<TokenTree> = root.filter([Arrow], ALL);
+		for (ar in allArrows) {
+			Assert.areEqual(ArrowType.FUNCTION_TYPE_HAXE3, TokenTreeCheckUtils.getArrowType(ar));
+		}
+	}
+
+	@Test
+	public function testFunctionTypeHaxe4() {
+		var root:TokenTree = assertCodeParses(TokenTreeCheckUtilsTests.FUNCTION_TYPE_HAXE_4);
+
+		var allArrows:Array<TokenTree> = root.filter([Arrow], ALL);
+		for (ar in allArrows) {
+			Assert.areEqual(ArrowType.FUNCTION_TYPE_HAXE4, TokenTreeCheckUtils.getArrowType(ar));
+		}
+	}
+
 	public function assertCodeParses(code:String, ?pos:PosInfos):TokenTree {
 		var builder:TestTokenTreeBuilder = null;
 		try {
@@ -83,6 +114,43 @@ abstract TokenTreeCheckUtilsTests(String) to String {
 	}
 	enum Main {
 		Test;
+	}
+	";
+
+	var ARROW_FUNCTIONS = "
+	class Main {
+		static public function main() {
+			// arrow functions
+			var f = () ->trace('');
+			var f = () -> {};
+			var f = arg-> {};
+			var f = (arg) -> {};
+			var f = (arg1:Int, arg2:String) -> {};
+		}
+	}
+	";
+
+	var FUNCTION_TYPE_HAXE_3 = "
+	class Main {
+		static public function main() {
+			// old function type syntax
+			var f:Void->Void;
+			var f:Int->String->Void;
+			var f:(Int->Int) ->Int->Int;
+		}
+	}
+	";
+
+	var FUNCTION_TYPE_HAXE_4 = "
+	class Main {
+		static public function main() {
+			// new function type syntax
+			var f:() ->Void;
+			var f:(Int) ->Int;
+			var f:(name:String) ->Void;
+			var f:(Int, String) ->Bool;
+			var f:(resolve:(value:Dynamic) ->Void, reject:(reason:Dynamic) ->Void) ->Void;
+		}
 	}
 	";
 }
