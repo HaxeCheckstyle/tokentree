d03c49c016eae014dc7e7b0e9d00738def66f88a
AlexHaxe <Alexander.Blum@gmail.com>
2018-07-27 23:52:23 +0200

    added unittest

--- test/tokentree/utils/TokenTreeCheckUtilsTest.hx
+++ test/tokentree/utils/TokenTreeCheckUtilsTest.hx
@@ -21,13 +21,14 @@ class TokenTreeCheckUtilsTest {
 	public function testMixedBrOpenTypes() {
 		var root:TokenTree = assertCodeParses(TokenTreeCheckUtilsTests.MIXED_BR_OPEN_TYPES);
 		var allBr:Array<TokenTree> = root.filter([BrOpen], ALL);
-		Assert.areEqual(6, allBr.length);
+		Assert.areEqual(7, allBr.length);
 		Assert.areEqual(BrOpenType.BLOCK, TokenTreeCheckUtils.getBrOpenType(allBr[0]));
 		Assert.areEqual(BrOpenType.ANONTYPE, TokenTreeCheckUtils.getBrOpenType(allBr[1]));
 		Assert.areEqual(BrOpenType.ANONTYPE, TokenTreeCheckUtils.getBrOpenType(allBr[2]));
 		Assert.areEqual(BrOpenType.BLOCK, TokenTreeCheckUtils.getBrOpenType(allBr[3]));
 		Assert.areEqual(BrOpenType.OBJECTDECL, TokenTreeCheckUtils.getBrOpenType(allBr[4]));
 		Assert.areEqual(BrOpenType.OBJECTDECL, TokenTreeCheckUtils.getBrOpenType(allBr[5]));
+		Assert.areEqual(BrOpenType.BLOCK, TokenTreeCheckUtils.getBrOpenType(allBr[6]));
 	}
 
 	@Test
@@ -101,6 +102,10 @@ abstract TokenTreeCheckUtilsTests(String) to String {
 	class Main {
 		function foo(s:{i:Int}):{i:Int} {
 			call({s: {i: 5}});
+			var foo = {
+				var bar = 'foo ';
+				bar;
+			}
 		}
 	}
 	";
