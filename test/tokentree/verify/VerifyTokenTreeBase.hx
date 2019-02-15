ae95aa9bc64e301c1237c97225fa95ad76fd0b80
AlexHaxe <Alexander.Blum@gmail.com>
2019-02-15 19:51:34 +0100

    fixed popen handling and detection (#141)
    * fixed popen handling and detection

--- test/tokentree/verify/VerifyTokenTreeBase.hx
+++ test/tokentree/verify/VerifyTokenTreeBase.hx
@@ -1,15 +1,16 @@
 package tokentree.verify;
 
+import haxe.PosInfos;
 import massive.munit.Assert;
 import tokentree.TokenTree;
 import tokentree.walk.WalkFile;
 
 class VerifyTokenTreeBase {
-	function buildTokenTree(content:String):IVerifyTokenTree {
+	function buildTokenTree(content:String, ?pos:PosInfos):IVerifyTokenTree {
 		var builder:TestTokenTreeBuilder = new TestTokenTreeBuilder(content);
 		var root:TokenTree = new TokenTree(null, "", null, -1);
 		walkStream(builder.getTokenStream(), root);
-		Assert.isTrue(builder.isStreamEmpty(), "there are still unused tokens in stream!!");
+		Assert.isTrue(builder.isStreamEmpty(), "there are still unused tokens in stream!!", pos);
 		return new VerifyTokenTree(root);
 	}
 
