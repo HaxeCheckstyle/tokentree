3067f45d610579a962859a3cc85ae077a2afc2a4
AlexHaxe <Alexander.Blum@gmail.com>
2019-05-30 20:20:15 +0200

    fixed conditionals using dot idents without parens (#159)
    * fixed conditionals using dot idents without parens

--- src/tokentree/walk/WalkSharp.hx
+++ src/tokentree/walk/WalkSharp.hx
@@ -110,6 +110,18 @@ class WalkSharp {
 				case Kwd(_), Const(CIdent(_)):
 					childToken = stream.consumeToken();
 					parent.addChild(childToken);
+					if (!stream.hasMore()) return;
+					switch (stream.token()) {
+						case Dot:
+						default: return;
+					}
+					var pos:Null<Position> = stream.getTokenPos();
+					if (pos == null) return;
+					if (pos.min == childToken.pos.max + 1) continue;
+
+					var dot:TokenTree = stream.consumeToken();
+					childToken.addChild(dot);
+					WalkSharp.walkSharpIfExpr(stream, dot);
 					return;
 				default:
 					return;
