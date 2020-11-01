4f6d30f531a34998ecba0e84a5395903e683d45b
AlexHaxe <Alexander.Blum@gmail.com>
2020-11-01 02:34:03 +0100

    cleanup

--- src/tokentree/walk/WalkQuestion.hx
+++ src/tokentree/walk/WalkQuestion.hx
@@ -20,29 +20,29 @@ class WalkQuestion {
 	public static function isTernary(parent:TokenTree):Bool {
 		var lastChild:Null<TokenTree> = parent.getLastChild();
 		if (lastChild == null) {
-			return switch (parent.tok) {
-				case Const(_): true;
-				default: false;
+			switch (parent.tok) {
+				case Const(_):
+					return true;
+				default:
+					lastChild = parent;
 			}
 		}
-		else {
-			return switch (lastChild.tok) {
-				case Const(_): true;
-				case BkOpen: true;
-				case BrOpen: true;
-				case Binop(OpAdd), Binop(OpSub): true;
-				case Unop(_): true;
-				case Kwd(KwdCast): true;
-				case Kwd(KwdNew): true;
-				case Kwd(KwdTrue), Kwd(KwdFalse), Kwd(KwdNull): true;
-				case Kwd(KwdThis), Kwd(KwdMacro), Kwd(KwdUntyped): true;
-				case Kwd(KwdFunction): true;
-				case Dollar(_): true;
-				case POpen: true;
-				case PClose: true;
-				case DblDot: true;
-				default: false;
-			}
+		return switch (lastChild.tok) {
+			case Const(_): true;
+			case BkOpen: true;
+			case BrOpen: true;
+			case Binop(OpAdd), Binop(OpSub): true;
+			case Unop(_): true;
+			case Kwd(KwdCast): true;
+			case Kwd(KwdNew): true;
+			case Kwd(KwdTrue), Kwd(KwdFalse), Kwd(KwdNull): true;
+			case Kwd(KwdThis), Kwd(KwdMacro), Kwd(KwdUntyped): true;
+			case Kwd(KwdFunction): true;
+			case Dollar(_): true;
+			case POpen: true;
+			case PClose: true;
+			case DblDot: true;
+			default: false;
 		}
 	}
 }
